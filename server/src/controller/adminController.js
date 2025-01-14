d531a19e2ea93bcb4050d1a29a4f889ce7cda3a3
[bisha21] <[timilsinab22bi@gmail.com]>
2025-01-14 12:43:19 +0545

    removing student and admin features based on our UML

--- server/src/controller/adminController.js
+++
@@ -1,7 +0,0 @@
-import Admin from "../model/adminModel.js";
-import { createOne, deleteOne, getAll, updateOne } from "./handleFactoryController.js";
-
-export const createAdmin=  createOne(Admin);
-export const getAllAdmin= getAll(Admin);
-export const updateAdmin= updateOne(Admin);
-export const deleteAdmin= deleteOne(Admin);
