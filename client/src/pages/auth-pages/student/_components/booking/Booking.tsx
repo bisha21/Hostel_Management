925fae779e58b250d3a1f38bd2f48c9bf70e7bff
[bisha21] <[timilsinab22bi@gmail.com]>
2025-01-20 15:47:51 +0545

    `feat: Added useCreateBooking mutation and updated related queries and components`

--- client/src/pages/auth-pages/student/_components/booking/Booking.tsx
+++ client/src/pages/auth-pages/student/_components/booking/Booking.tsx
@@ -1,13 +1,26 @@
 import { ArrowRight } from "lucide-react";
 import { Button } from "../../../../../components/ui/button";
+import { useCreateBooking } from "../../../../../api/mutations/room.mutation";
 
-const Booking = () => {
+type BookingProps = {
+  id: number;
+};
+
+const Booking = ({ id }: BookingProps) => {
+  const { mutate: createBooking, isLoading } = useCreateBooking();
+
+  const handleBooking = () => {
+    createBooking(id);
+  };
+
   return (
     <div className="flex flex-col gap-5 items-center justify-center">
-      <h2 className="text-5xl  text-orange-400 font-semibold text-center ">
+      <h2 className="text-5xl text-orange-400 font-semibold text-center">
         Book today. Pay on arrival.
       </h2>
-      <Button >Book Now <ArrowRight/></Button>
+      <Button onClick={handleBooking} disabled={isLoading}>
+        {isLoading ? "Booking..." : "Book Now"} <ArrowRight />
+      </Button>
     </div>
   );
 };
