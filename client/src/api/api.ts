0522dc0904a01e3591e6e542c08ec1ef211a0cfd
[bisha21] <[timilsinab22bi@gmail.com]>
2025-03-25 20:21:21 +0545

    reset password in frontend

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,22 +1,23 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
-    headers:{
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("authToken")}`
-    },
-})
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+  },
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("authToken")
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
