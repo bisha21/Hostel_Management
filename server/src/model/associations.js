8dabcb08ab00e42201db1d8272ea0aa78c417d5b
[bisha21] <[timilsinab22bi@gmail.com]>
2025-04-20 08:45:25 +0545

    `Added associations for Notification and User models`

--- server/src/model/associations.js
+++ server/src/model/associations.js
@@ -1,8 +1,11 @@
 import Room from "./RoomModal.js";
 import Booking from "./bookingModel.js";
+import Notification from "./notificationModel.js";
+import User from "./userModal.js";
 
 Room.hasMany(Booking, { foreignKey: "roomId", as: "bookings" });
 Booking.belongsTo(Room, { foreignKey: "roomId", as: "room" });
+User.hasMany(Notification, { foreignKey: "userId" });
+Notification.belongsTo(User, { foreignKey: "userId" });
 
-
-export default { Room, Booking };
+export default { Room, Booking, Notification, User };
