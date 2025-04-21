70d032ccdf47ab8e3cccf8288016a02b7cd1aad4
tooti-frooti <nischalbh51@gmail.com>
2025-04-21 07:38:12 +0545

    fix: user notification

--- server/src/routes/notificationRoutes.js
+++ server/src/routes/notificationRoutes.js
@@ -1,10 +1,15 @@
-import express from 'express';
-import { protectedRoutes } from '../middleware/protectRoute.js';
-import { createNotification, createNotificationForAll, getAllNotification, getMyNotifications } from '../controller/notificationController.js';
+import express from "express";
+import { protectedRoutes } from "../middleware/protectRoute.js";
+import {
+  createNotification,
+  createNotificationForAll,
+  getAllNotification,
+  getMyNotifications,
+} from "../controller/notificationController.js";
 const router = express.Router();
 // router.use(protectedRoutes);
-router.post('/',createNotification);
-router.get('/', getAllNotification);
-router.post('/all',createNotificationForAll);
-router.get('/own',getMyNotifications);
+router.post("/", createNotification);
+router.get("/", getAllNotification);
+router.post("/all", createNotificationForAll);
+router.get("/own", protectedRoutes, getMyNotifications);
 export default router;
