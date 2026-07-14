b0562dce036713c870d9ea14caea8a8675667dc6
[bisha21] <[timilsinab22bi@gmail.com]>
2026-07-14 22:44:15 +0545

    fix: update backend URL in .env and allow all origins in CORS configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -41,7 +41,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PATCH", "OPTIONS", "PUT"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
